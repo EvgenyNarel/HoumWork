5a94a8d4c4cfa0c9186c5384bf711d72aa74866b
EvgenyNarel <67507704+EvgenyNarel@users.noreply.github.com>
2020-07-11 23:06:06 +0300

    Update Bater.java

--- Bater.java
+++ Bater.java
@@ -8,32 +8,25 @@ public class Bater {
         int m = 5;
         int arr[][] = new int[n][m];
 
-        for (int i = 0; i < arr.length; i++) {
+
+        for (int i = 0; i < arr.length; i++) {//
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + "\t");
+                //System.out.print(arr[i][j] + "\t");
+                if(i<=j&&j<arr[i].length-i||i==arr[i].length-1||i>=arr[i].length-i&&j<=i&&j>=arr[i].length-i-1) {
+                    arr[i][j] = 1;
+                }
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
 
-                if (i == 0 || i == m - 1) {
-                    arr[i][j] = 1;
-                    }
-               else if((i==1||i==n-2)&&(j>=1&&j<=m-2)){
-                   arr[i][j]=1;}
-                   else if((i==2||i==n-3)&&(j>=2&&j<=m-3)){
-                       arr[i][j]=1;}
-                }
-        }
             System.out.println();
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr[i].length; j++) {
-              System.out.print(arr[i][j] + "\t");
-                }
-                System.out.println();
-
+            for(int i = 0;i<arr.length;i++)
+    {
+        for (int j = 0; j < arr[i].length; j++) {
+            System.out.print(arr[i][j] + "\t");
         }
+        System.out.println();
+
+
     }
 }
-
-
+        }
