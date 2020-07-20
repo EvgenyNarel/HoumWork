a7ac24f450b76da9608f4e743dbce9fa0ec4bff9
EvgenyNarel <narel40@mail.ru>
2020-07-20 22:18:08 +0300

    Lesson3_fixses3 ThreeNumber

--- ThreeNumber.java
+++ ThreeNumber.java
@@ -2,14 +2,18 @@ public class ThreeNumber {
     public static void main(String[] args) {
         /*Найдите сумму первых n целых чисел, которые делятся на 3.
          */
-
+        int number = 2;
         int sum = 0;
-        int i = 0;
-        for(int j=0;j<3;j++) {
-            if (i % 3 == 0) {
-                sum += i;
+        int counter=0;
+        for (int j = 1; j > 0; j++) {
+            if (j % 3 == 0) {
+                sum += j;
+                counter++;
             }
-            i += 3;
+            if (counter == number) {
+                break;
+
+            }
 
         }
         System.out.println(sum);
